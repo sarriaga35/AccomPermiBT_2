44e034627daf8a01ff2fd3a9fbfbab8f8a51e6f1
sarriaga35 <sebasarriaga@gmail.com>
2024-06-18 21:08:04 -0600

    Version 2.0 Basico, con Accompanist Library y actualizaciones 18 Junio <30 BLUETOOTH

--- app/src/main/java/com/sebas/accompermibt_2/MainActivity.kt
+++ app/src/main/java/com/sebas/accompermibt_2/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.sebas.accompermibt_2
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -13,25 +18,87 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.sebas.accompermibt_2.ui.theme.AccomPermiBT_2Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+
             AccomPermiBT_2Theme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Sample()
+                val versionSDK30 = listOf(android.Manifest.permission.BLUETOOTH)
+                val versionSDK31 = listOf(android.Manifest.permission.BLUETOOTH_CONNECT, android.Manifest.permission.BLUETOOTH_SCAN)
+                val multiplePermissionsState = rememberMultiplePermissionsState(
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) versionSDK31 else versionSDK30
+                )
+                Sample(multiplePermissionsState)
                 }
             }
         }
     }
-    @Composable
-    private fun Sample() {
-        Text(text = "Hello World!")
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun Sample(multiplePermissionsState: MultiplePermissionsState) {
+    if (multiplePermissionsState.allPermissionsGranted) {
+        // If all permissions are granted, then show screen with the feature enabled
+        Text("BT permissions Granted! Thank you!")
+    } else {
+        Column {
+            Text(
+                getTextToShowGivenPermissions(
+                    multiplePermissionsState.revokedPermissions,
+                    multiplePermissionsState.shouldShowRationale
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
+                Text("Request permissions")
+            }
+        }
     }
+}
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+private fun getTextToShowGivenPermissions(
+    permissions: List<PermissionState>,
+    shouldShowRationale: Boolean
+): String {
+    val revokedPermissionsSize = permissions.size
+    if (revokedPermissionsSize == 0) return ""
+
+    val textToShow = StringBuilder().apply {
+        append("The ")
+    }
+
+    for (i in permissions.indices) {
+        textToShow.append(permissions[i].permission)
+        when {
+            revokedPermissionsSize > 1 && i == revokedPermissionsSize - 2 -> {
+                textToShow.append(", and ")
+            }
+            i == revokedPermissionsSize - 1 -> {
+                textToShow.append(" ")
+            }
+            else -> {
+                textToShow.append(", ")
+            }
+        }
+    }
+    textToShow.append(if (revokedPermissionsSize == 1) "permission is" else "permissions are")
+    textToShow.append(
+        if (shouldShowRationale) {
+            " important. Please grant all of them for the app to function properly."
+        } else {
+            " denied. The app cannot function without them."
+        }
+    )
+    return textToShow.toString()
 }
