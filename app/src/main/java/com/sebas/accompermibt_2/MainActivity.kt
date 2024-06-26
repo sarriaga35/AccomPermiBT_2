99e19edd22879f5df02ba953d16a80fd93a1148f
sarriaga35 <sebasarriaga@gmail.com>
2024-06-26 09:15:35 -0600

    Version 4.0 No tan Basico, con Accompanist Library y actualizaciones 26 Junio <30 BLUETOOTH Incluida permisiondialog con 3 idiomas, color, theme, typo. appsettings

--- app/src/main/java/com/sebas/accompermibt_2/MainActivity.kt
+++ app/src/main/java/com/sebas/accompermibt_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sebas.accompermibt_2
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -22,15 +23,21 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.sebas.accompermibt_2.ui.theme.AccomPermiBT_2Theme
 
+
+
+
 class MainActivity : ComponentActivity() {
+
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,38 +47,35 @@ class MainActivity : ComponentActivity() {
             AccomPermiBT_2Theme {
                 val versionSDK30 = listOf(android.Manifest.permission.BLUETOOTH)
                 val versionSDK31 = listOf(android.Manifest.permission.BLUETOOTH_CONNECT, android.Manifest.permission.BLUETOOTH_SCAN)
-                val multiplePermissionsState = rememberMultiplePermissionsState(
+                val BTPermissionsState = rememberMultiplePermissionsState(
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) versionSDK31 else versionSDK30
                 )
-                Sample(multiplePermissionsState)
+                if (!BTPermissionsState.allPermissionsGranted) {
+                    // If all permissions are granted, then show screen with the feature enabled
+                    Text("BT permissions Granted! Thank you!")
+                } else {
+                    PermissionDialog(
+                        permission =if (BTPermissionsState.shouldShowRationale) {
+                            "Al parecer ud declino el permiso del Bluetooth, permanentemente " +
+                                    "Ud. puede ir a la configuracion del telefono y otorgar el permiso."
+                        } else {
+                            "Esta app requiere el Bluetooth para comunicarse con el vehiculo "
+                        }
+
+                        ,
+                        isPermanentlyDeclined= !BTPermissionsState.shouldShowRationale,
+                        onDismiss = {/*viewModel::dismissDialog*/},
+                        onOkClick = {BTPermissionsState.launchMultiplePermissionRequest()},
+                        onGoToAppSettingsClick = ::openAppSettings
+                    )
+
+                }
                 }
             }
         }
     }
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-private fun Sample(multiplePermissionsState: MultiplePermissionsState) {
-    if (multiplePermissionsState.allPermissionsGranted) {
-        // If all permissions are granted, then show screen with the feature enabled
-        Text("BT permissions Granted! Thank you!")
-    } else {
-        PermissionDialog(
-            permission =if (isPermanentlyDeclined) {
-                "Al parecer ud declino el permiso del Bluetooth, permanentemente " +
-                        "Ud. puede ir a la configuracion del telefono y otorgar el permiso."
-            } else {
-                "Esta app requiere el Bluetooth para comunicarse con el vehiculo "
-            }
-                    isPermanentlyDeclined =!shouldShowRequestPermissionRationale(
-                Manifest.permission.CAMERA
-            ),
-            onDismiss = {/*viewModel::dismissDialog*/},
-            onOkClick = {/*  viewModel.dismissDialog()*/
-            },
-            onGoToAppSettingsClick = ::openAppSettings
-        )
-    }
-}
+
+
 
 
 
@@ -81,6 +85,10 @@ fun Activity.openAppSettings() {
         Uri.fromParts("package", packageName, null)
     ).also(::startActivity)
 }
+
+
+
+
 
 
 
--- app/src/main/java/com/sebas/accompermibt_2/PermissionDialog.kt
+++ app/src/main/java/com/sebas/accompermibt_2/PermissionDialog.kt
@@ -1,9 +1,16 @@
 package com.sebas.accompermibt_2
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PermissionDialog (
@@ -15,8 +22,59 @@ fun PermissionDialog (
     modifier: Modifier = Modifier
 ) {
 
-AlertDialog(onDismissRequest = onDismiss,
-    title = { Text(text ="Permiso Requerido"  ) },
-    text = {  Text(text = permission) },
-    confirmButton = {  if (isPermanentlyDeclined) { onGoToAppSettingsClick() } else {onOkClick()} })
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(),
+        title = {
+            Text(
+                text = stringResource(R.string.permision),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            )
+        },
+
+        text = {
+            Text(
+                text = permission,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            )
+        },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+//            if (isPermanentlyDeclined) {
+//                onGoToAppSettingsClick()
+//            } else {
+//                onOkClick()
+//            }
+            TextButton(
+                onClick = { onOkClick},
+                elevation = ButtonDefaults.elevatedButtonElevation(2.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.confirmation),
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismiss },
+                elevation = ButtonDefaults.elevatedButtonElevation(2.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.dimiss),
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+        }
+    )
 }
